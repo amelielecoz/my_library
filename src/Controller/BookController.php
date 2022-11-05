ff978af8a5e84e8fdb2329841afd09511653a5b8
Amelie Le Coz <lecozamelie@hotmail.com>
2022-11-05 19:57:06 +0100

    get book data from DB

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,65 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
+    public function __construct(private BookRepository $bookRepository)
+    {
+    }
+
     #[Route('/book', name: 'app_book')]
     public function index(): Response
     {
-        $books = [
-            [
-                "title" => "Title 1",
-                "image" => "https://picsum.photos/200/300",
-                "isbn" => 2266211099,
-                "isbn-13" => 9782266211093,
-                "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                "available" => true,
-            ],
-            [
-                "title" => "Title 2",
-                "image" => "https://picsum.photos/200/300",
-                "isbn" => 2266211099,
-                "isbn-13" => 9782266211093,
-                "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                "available" => true,
-            ],
-            [
-                "title" => "Title 3",
-                "image" => "https://picsum.photos/200/300",
-                "isbn" => 2266211099,
-                "isbn-13" => 9782266211093,
-                "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                "available" => true,
-            ],
-            [
-                "title" => "Title 4",
-                "image" => "https://picsum.photos/200/300",
-                "isbn" => 2266211099,
-                "isbn-13" => 9782266211093,
-                "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                "available" => true,
-            ],
-            [
-                "title" => "Title 5",
-                "image" => "https://picsum.photos/200/300",
-                "isbn" => 2266211099,
-                "isbn-13" => 9782266211093,
-                "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                "available" => true,
-            ],
-            [
-                "title" => "Title 6",
-                "image" => "https://picsum.photos/200/300",
-                "isbn" => 2266211099,
-                "isbn-13" => 9782266211093,
-                "summary" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                "available" => true,
-            ],
-        ];
+        $books = $this->bookRepository->findAll();
 
         return $this->render('book/index.html.twig', [
             'controller_name' => 'BookController',
