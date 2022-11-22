6465b883b0b22b2d97493f57689518eaaff33ec2
Amelie Le Coz <lecozamelie@hotmail.com>
2022-11-22 10:27:24 +0100

    Add comment fixtures

---
+++ src/DataFixtures/CommentFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Comment;
+use App\Repository\BookRepository;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class CommentFixtures extends Fixture
+{
+    public function __construct(private readonly BookRepository $bookRepository)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $books = $this->bookRepository->findAll();
+
+        foreach ($books as $book) {
+            $comment = new Comment();
+            $comment->setTitle($faker->sentence($faker->numberBetween(3, 7)));
+            $comment->setText($faker->paragraph(5));
+            $comment->setAuthor($faker->name());
+            $comment->setBook($book);
+
+            $dateTimeImmutable = \DateTimeImmutable::createFromMutable($faker->dateTimeBetween('- 1 years'));
+            $comment->setCreatedAt($dateTimeImmutable);
+
+            $manager->persist($comment);
+        }
+
+        $manager->flush();
+    }
+}
