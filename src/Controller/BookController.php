3c1774d41249bb5e9b2c2245f178c73ca6bc7f14
Amelie Le Coz <lecozamelie@hotmail.com>
2022-11-25 10:01:25 +0100

    add paginator

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -8,6 +8,7 @@ use App\Entity\ReservationRequest;
 use App\Form\CommentFormType;
 use App\Form\ReservationRequestFormType;
 use App\Repository\BookRepository;
+use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,8 +20,10 @@ use Twig\Environment;
 class BookController extends AbstractController
 {
     public function __construct(
-        private readonly BookRepository $bookRepository,
-        private EntityManagerInterface $entityManager,
+        private readonly BookRepository         $bookRepository,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly Environment            $twig,
+        private readonly CommentRepository      $commentRepository
     )
     {
     }
@@ -30,15 +33,22 @@ class BookController extends AbstractController
     {
         $books = $this->bookRepository->findAll();
 
-        return $this->render('book/index.html.twig', [
+        return new Response($this->twig->render('book/index.html.twig', [
             'controller_name' => 'BookController',
             'books' => $books,
-        ]);
+        ]));
     }
 
     #[Route('/book/{slug}', name: 'app_book_show')]
-    public function show(Request $request, Environment $twig, Book $book, SpamChecker $spamChecker): Response
+    public function show(
+        Request $request,
+        Book $book,
+        SpamChecker $spamChecker,
+    ): Response
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $this->commentRepository->getCommentPaginator($book, $offset);
+
         $comment = new Comment();
         $commentForm = $this->createForm(CommentFormType::class, $comment);
         $commentForm->handleRequest($request);
@@ -74,10 +84,13 @@ class BookController extends AbstractController
             return $this->redirectToRoute('app_book_show', ['slug' => $book->getSlug()]);
         }
 
-        return new Response($twig->render('book/show.html.twig', [
+        return new Response($this->twig->render('book/show.html.twig', [
             'book' => $book,
             'comment_form' => $commentForm->createView(),
             'reservation_request_form' => $reservationRequestForm->createView(),
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
 }
